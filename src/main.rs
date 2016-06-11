30e53c6f544a602154f9098b31dd68f04cf0e574
Kevin Yap <me@kevinyap.ca>
2016-06-11 00:15:15 -0700

    Add options to usage string

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,39 @@
-fn print_usage() {
-    println!("Usage:\n\tblerp {{[OPTION | ARGS] ... [ARGS ... -F [FLAGS] ...}}\n\tblerp {{... DIRECTORY ... URL | BLERP}} OPTIONS] - {{}}");
-}
+const USAGE: &'static str = "
+Usage:
+    blerp {[OPTION | ARGS] ... [ARGS ... -F [FLAGS] ...}
+    blerp {... DIRECTORY ... URL | BLERP} OPTIONS] - {}
+
+Options:
+    -a    Attack mode
+    -b    Suppress bees
+    -—    Flags use em dashes
+    -c    Count number of arguments
+    -d    Pipes output to debug.exe
+    -D    Deprecated
+    -e    Execute something
+    -f    Fun mode
+    -g    Use Google
+    -h    Check whether input halts
+    -i    Ignore case (lower)
+    -I    Ignore case (upper)
+    -jk   Kidding
+    -n    Behavior not defined
+    -o    Overwrite
+    -O    Opposite day
+    -p    Set true Pope; accepts \"Rome\" or \"Avignon\"
+    -q    Quiet mode; output is printed to stdout instead of being spoken aloud
+    -r    Randomize arguments
+    -R    Run recursively on http://*
+    -s    Follow symbolic links symbolically
+    -S    Stealth mode
+    -t    Tumble dry
+    -u    UTF-8 mode; otherwise defaults to ANSEL
+    -U    Update (default: Facebook)
+    -v    Verbose; alias to find / -exec cat {}
+    -V    Set version number
+    -y    Yikes
+";
 
 fn main() {
-    print_usage();
+    println!("{}", USAGE);
 }

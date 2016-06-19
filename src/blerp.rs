4f92d9952ae8b9e04b1152b6cd60d1e73c7e7819
Kevin Yap <me@kevinyap.ca>
2016-06-18 20:53:12 -0700

    Move main program logic to Blerp::run()

--- src/blerp.rs
+++ src/blerp.rs
@@ -1,4 +1,14 @@
-use docopt::{ArgvMap, Docopt};
+use std::fs;
+use std::io;
+use std::thread;
+
+use std::io::Write;
+use std::process::Command;
+use std::time::Duration;
+
+use ansi_term::Style;
+use ansi_term::Colour::Red;
+use docopt::Docopt;
 
 const USAGE: &'static str = "
 blerp filters local or remote files or resources.
@@ -40,29 +50,30 @@ Options:
 
 #[derive(Debug)]
 pub struct Blerp {
-    pub arguments: Vec<String>,
-    pub suppress_bees: bool,
-    pub count_args: bool,
-    pub deprecated: bool,
-    pub fun_mode: bool,
-    pub use_google: bool,
-    pub check_if_halts: bool,
-    pub opposite_day: bool,
-    pub quiet_mode: bool,
-    pub stealth_mode: bool,
+    arguments: Vec<String>,
+    suppress_bees: bool,
+    count_args: bool,
+    deprecated: bool,
+    fun_mode: bool,
+    use_google: bool,
+    check_if_halts: bool,
+    opposite_day: bool,
+    quiet_mode: bool,
+    stealth_mode: bool,
 }
 
 impl Blerp {
-    pub fn new() -> Self {
+    pub fn new<S>(argv: Vec<S>) -> Result<Self, String> where S: AsRef<str> {
         let version = Some(format!("{} {}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION")));
-        let argvmap: ArgvMap = Docopt::new(USAGE)
-                                      .unwrap_or_else(|e| e.exit())
-                                      .options_first(true)
-                                      .version(version)
-                                      .parse()
-                                      .unwrap_or_else(|e| e.exit());
+        let argvmap = Docopt::new(USAGE)
+                             .unwrap_or_else(|e| e.exit())
+                             .argv(argv)
+                             .options_first(true)
+                             .version(version)
+                             .parse()
+                             .unwrap_or_else(|e| e.exit());
 
-        Blerp {
+        let blerp = Blerp {
             arguments:      argvmap.get_vec("<path>").iter().map(|a| a.to_string()).collect::<Vec<String>>(),
             suppress_bees:  argvmap.get_bool("-b"),
             count_args:     argvmap.get_bool("-c"),
@@ -73,6 +84,106 @@ impl Blerp {
             opposite_day:   argvmap.get_bool("-O"),
             quiet_mode:     argvmap.get_bool("-q"),
             stealth_mode:   argvmap.get_bool("-S"),
+        };
+
+        Ok(blerp)
+    }
+
+    pub fn run(&self) {
+        // Use Google
+        if self.use_google {
+            let engine = if self.opposite_day {
+                "https://duckduckgo.com/"
+            } else {
+                "https://www.google.com/search"
+            };
+
+            let url = format!("{}?q={}", engine, self.arguments.join("+"));
+            Command::new("open").arg(url).spawn().expect("failed to open");
+        }
+
+        // Count number of arguments
+        if self.count_args {
+            println!("Number of arguments: {}", self.arguments.len());
+        }
+
+        // Deprecated feature
+        if self.deprecated {
+            println!("{}", Red.paint("Use of the `-D` option is deprecated."));
+        }
+
+        // Fun mode
+        if self.fun_mode {
+            println!("{}", Style::new().blink().bold().paint("Fun mode!"));
+        }
+
+        let mut files: Vec<String> = Vec::new();
+
+        for path in fs::read_dir("./").unwrap() {
+            if path.as_ref().unwrap().metadata().unwrap().is_file() {
+                files.push(path.unwrap().file_name().into_string().unwrap());
+            }
+        }
+
+        // Stealth mode
+        let mut style = Style::new();
+
+        if self.stealth_mode {
+            if self.opposite_day {
+                style = style.bold();
+            } else {
+                style = style.dimmed();
+            }
+        }
+
+        // Determine if `say` or `espeak` are available
+        let say_cmd: Option<&str>;
+
+        if Command::new("say").spawn().is_ok() {
+            say_cmd = Some("say");
+        } else if Command::new("espeak").spawn().is_ok() {
+            say_cmd = Some("espeak");
+        } else {
+            say_cmd = None;
+
+            if self.quiet_mode && self.opposite_day {
+                println!("`say` and `espeak` are unavailable. Defaulting to quiet mode.")
+            }
+        }
+
+        for mut file in files {
+            // Suppress bees
+            if self.suppress_bees {
+                file = file.replace("B", "Ƀ").replace("b", "ƀ");
+            }
+
+            // Quiet mode, opposite day
+            if self.quiet_mode && self.opposite_day && say_cmd.is_some() {
+                Command::new(String::from(say_cmd.unwrap())).arg(file).output().unwrap();
+            } else {
+                println!("{}", style.paint(file));
+            }
+        }
+
+        // Check whether input halts
+        if self.check_if_halts {
+            if self.opposite_day {
+                println!("Input halts.");
+            } else {
+                // TODO: Solve Halting problem
+                print!("Checking whether input halts");
+                let (mut delay, mut next) = (1, 1);
+
+                loop {
+                    io::stdout().flush().unwrap();
+                    print!(".");
+                    thread::sleep(Duration::from_secs(delay));
+
+                    let tmp = delay + next;
+                    delay = next;
+                    next = tmp;
+                }
+            }
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,114 +2,18 @@ extern crate ansi_term;
 extern crate docopt;
 extern crate rustc_serialize;
 
-use std::fs;
-use std::io;
-use std::io::Write;
-use std::process::Command;
-use std::thread;
-use std::time::Duration;
-
-use ansi_term::Style;
-use ansi_term::Colour::Red;
+use std::process;
 
 mod blerp;
 
 fn main() {
-    let blerp = blerp::Blerp::new();
+    let argv: Vec<String> = std::env::args().collect();
 
-    // Use Google
-    if blerp.use_google {
-        let engine = if blerp.opposite_day {
-            "https://duckduckgo.com/"
-        } else {
-            "https://www.google.com/search"
-        };
-
-        let url = format!("{}?q={}", engine, blerp.arguments.join("+"));
-        Command::new("open").arg(url).spawn().expect("failed to open");
-    }
-
-    // Count number of arguments
-    if blerp.count_args {
-        println!("Number of arguments: {}", blerp.arguments.len());
-    }
-
-    // Deprecated feature
-    if blerp.deprecated {
-        println!("{}", Red.paint("Use of the `-D` option is deprecated."));
-    }
-
-    // Fun mode
-    if blerp.fun_mode {
-        println!("{}", Style::new().blink().bold().paint("Fun mode!"));
-    }
-
-    let mut files: Vec<String> = Vec::new();
-
-    for path in fs::read_dir("./").unwrap() {
-        if path.as_ref().unwrap().metadata().unwrap().is_file() {
-            files.push(path.unwrap().file_name().into_string().unwrap());
-        }
-    }
-
-    // Stealth mode
-    let mut style = Style::new();
-
-    if blerp.stealth_mode {
-        if blerp.opposite_day {
-            style = style.bold();
-        } else {
-            style = style.dimmed();
-        }
-    }
-
-    // Determine if `say` or `espeak` are available
-    let say_cmd: Option<&str>;
-
-    if Command::new("say").spawn().is_ok() {
-        say_cmd = Some("say");
-    } else if Command::new("espeak").spawn().is_ok() {
-        say_cmd = Some("espeak");
-    } else {
-        say_cmd = None;
-
-        if blerp.quiet_mode && blerp.opposite_day {
-            println!("`say` and `espeak` are unavailable. Defaulting to quiet mode.")
-        }
-    }
-
-    for mut file in files {
-        // Suppress bees
-        if blerp.suppress_bees {
-            file = file.replace("B", "Ƀ").replace("b", "ƀ");
-        }
-
-        // Quiet mode, opposite day
-        if blerp.quiet_mode && blerp.opposite_day && say_cmd.is_some() {
-            Command::new(String::from(say_cmd.unwrap())).arg(file).output().unwrap();
-        } else {
-            println!("{}", style.paint(file));
-        }
-    }
-
-    // Check whether input halts
-    if blerp.check_if_halts {
-        if blerp.opposite_day {
-            println!("Input halts.");
-        } else {
-            // TODO: Solve Halting problem
-            print!("Checking whether input halts");
-            let (mut delay, mut next) = (1, 1);
-
-            loop {
-                io::stdout().flush().unwrap();
-                print!(".");
-                thread::sleep(Duration::from_secs(delay));
-
-                let tmp = delay + next;
-                delay = next;
-                next = tmp;
-            };
+    match blerp::Blerp::new(argv) {
+        Ok(blerp) => blerp.run(),
+        Err(msg) => {
+            println!("{}", msg);
+            process::exit(1);
         }
     }
 }

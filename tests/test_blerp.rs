81caffb09446f50748a93e39e7de07ea9b67c6e3
Kevin Yap <me@kevinyap.ca>
2016-06-20 00:13:58 -0700

    Add more integration tests

--- tests/test_blerp.rs
+++ tests/test_blerp.rs
@@ -6,3 +6,21 @@ fn test_blerp() {
     let blerp = Blerp::new(vec!["blerp"]);
     assert!(blerp.unwrap().run().is_ok());
 }
+
+#[test]
+fn test_help_option() {
+    let blerp = Blerp::new(vec!["blerp", "--help"]);
+    assert_eq!(blerp.unwrap_err().fatal(), false);
+}
+
+#[test]
+fn test_version_option() {
+    let blerp = Blerp::new(vec!["blerp", "--version"]);
+    assert_eq!(blerp.unwrap_err().fatal(), false);
+}
+
+#[test]
+fn test_invalid_option() {
+    let blerp = Blerp::new(vec!["blerp", "-k"]);
+    assert_eq!(blerp.unwrap_err().fatal(), true);
+}
